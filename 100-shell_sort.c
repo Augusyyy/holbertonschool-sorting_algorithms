4a035b862d69a6ce54e35fe36392471327d5d03e
Augusyyy <8851@holbertonstudents.com>
2024-04-14 00:29:13 +1000

    nweupdated

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * shell_sort - Sort an array of integers in ascending
  *              order using the shell sort algorithm.
  * @array: An array of integers.
@@ -10,22 +24,24 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int i, j, gap, temp;
+	size_t gap, i, j;
 
 	if (array == NULL || size < 2)
 		return;
-	for (gap = size / 2; gap > 0; gap /= 2)
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
 		{
-			temp = array[i];
 			j = i;
-			while (j >= gap && array[j - gap] > temp)
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				array[j] = array[j - gap];
+				swap_ints(array + j, array + (j - gap));
 				j -= gap;
 			}
-			array[j] = temp;
 		}
 		print_array(array, size);
 	}
--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,77 +1,68 @@
 #include "sort.h"
-/**
- * swap_102 - swap_102
- * @p1: p1
- * @p2: p2
- */
-void swap_102(int* p1, int* p2)
-{
-    int temp = *p1;
-    *p1 = *p2;
-    *p2 = temp;
-}
-/**
- * find_count_len - find_count_len
- * @a:
- * @len:
- * @return
- */
-int find_count_len(int* a, size_t len)
-{
-	int min = a[0], max = a[0];
-	int i;
-	int count = 0;
 
-	for (i = 0; i < len; i++)
+/**
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
 	{
-		count++;
-		if (a[i] > max)
-			max = a[i];
-		if (a[i] < min)
-			min = a[i];
+		if (array[i] > max)
+			max = array[i];
 	}
-	return max - min + 1;
+
+	return (max);
 }
+
 /**
  * counting_sort - Sort an array of integers in ascending order
  *                 using the counting sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
+ *
  * Description: Prints the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
-	int count_arr_len = find_count_len(array, size);
-	int* count_arr = (int*)malloc(sizeof(int) * count_arr_len);
-	size_t i;
-	int j, offset;
-	int *b;
+	int *count, *sorted, max, i;
 
-	for (i = 0; i < count_arr_len; i++)
-		count_arr[i] = 0;
+	if (array == NULL || size < 2)
+		return;
 
-	offset = array[0];
-	for (i = 1; i < size; i++)
-		if (array[i] < offset)
-			offset = array[i];
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
+		return;
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+	{
+		free(sorted);
+		return;
+	}
 
-	b = (int*)malloc(sizeof(int) * size);
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
 
-	for (i = 0; i < size; i++)
-		count_arr[array[i] - offset]++;
+	for (i = 0; i < (int)size; i++)
+	{
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
+	}
 
-	for (j = 1; j < count_arr_len; j++)
-		count_arr[j] += count_arr[j - 1];
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
 
-	for (i = size; i > 0; i--)
-		b[--count_arr[array[i - 1] - offset]] = array[i - 1];
-
-	print_array(count, offset + 1);
-	for (i = 0; i < size; i++)
-		swap_102(&array[i], &b[i]);
-
-	free(count_arr);
-	count_arr = NULL;
-	free(b);
-	b = NULL;
+	free(sorted);
+	free(count);
 }
